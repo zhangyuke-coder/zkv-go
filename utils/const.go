339d5cae974dcee701787a42b7e0f13c9c01c653
zhangyuke-coder <1034643857@qq.com>
2023-02-25 22:19:40 +0800

    success

---
+++ utils/const.go
@@ -0,0 +1,17 @@
+package utils
+
+import "hash/crc32"
+
+// meta
+const (
+	BitDelete       byte = 1 << 0 // Set if the key has been deleted.
+	BitValuePointer byte = 1 << 1 // Set if the value is NOT stored directly next to key.
+)
+
+// codec
+var (
+	MagicText    = [4]byte{'H', 'A', 'R', 'D'}
+	MagicVersion = uint32(1)
+	// CastagnoliCrcTable is a CRC32 polynomial table
+	CastagnoliCrcTable = crc32.MakeTable(crc32.Castagnoli)
+)
--- utils/key.go
+++ utils/key.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"encoding/binary"
 	"math"
+	"time"
 )
 
 func ParseKey(key []byte) []byte {
@@ -24,4 +25,21 @@ func ParseTs(key []byte) uint64 {
 		return 0
 	}
 	return math.MaxUint64 - binary.BigEndian.Uint64(key[len(key)-8:])
+}
+
+// KeyWithTs generates a new key by appending ts to key.
+func KeyWithTs(key []byte, ts uint64) []byte {
+	out := make([]byte, len(key)+8)
+	copy(out, key)
+	binary.BigEndian.PutUint64(out[len(key):], math.MaxUint64-ts)
+	return out
+}
+
+// SafeCopy does append(a[:0], src...).
+func SafeCopy(a, src []byte) []byte {
+	return append(a[:0], src...)
+}
+
+func NewCurVersion() uint64 {
+	return uint64(time.Now().UnixNano() / 1e9)
 }
