3ad1896023dac320f8219db7ec35ac2d0c2b5c55
zhangyuke-coder <1034643857@qq.com>
2023-03-25 11:35:14 +0800

    ting

--- utils/const.go
+++ utils/const.go
@@ -1,6 +1,10 @@
 package utils
 
-import "hash/crc32"
+import (
+	"hash/crc32"
+	"math"
+	"os"
+)
 
 // meta
 const (
@@ -14,4 +18,23 @@ var (
 	MagicVersion = uint32(1)
 	// CastagnoliCrcTable is a CRC32 polynomial table
 	CastagnoliCrcTable = crc32.MakeTable(crc32.Castagnoli)
+)
+
+// file
+const (
+	ManifestFilename                  = "MANIFEST"
+	ManifestRewriteFilename           = "REWRITEMANIFEST"
+	ManifestDeletionsRewriteThreshold = 10000
+	ManifestDeletionsRatio            = 10
+	DefaultFileFlag                   = os.O_RDWR | os.O_CREATE | os.O_APPEND
+	DefaultFileMode                   = 0666
+	MaxValueLogSize                   = 10 << 20
+	// This is O_DSYNC (datasync) on platforms that support it -- see file_unix.go
+	datasyncFileFlag = 0x0
+	// 基于可变长编码,其最可能的编码
+	MaxHeaderSize            = 21
+	VlogHeaderSize           = 0
+	MaxVlogFileSize   uint32 = math.MaxUint32
+	Mi                int64  = 1 << 20
+	KVWriteChCapacity        = 1000
 )
